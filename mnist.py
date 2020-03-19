8565dfbc43e95e984ea6e3c87df0c66a7860c6c8
Shreya Laddha <47536876+laddhashreya2000@users.noreply.github.com>
2020-03-19 23:29:48 +0530

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -5,17 +5,18 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train.shape, y_train.shape)
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_train = x_train.astype('float32')
+x_train /= 255
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-input_shape = (28, 28, 1)
+x_test = x_test.astype('float32')
+x_test /= 255
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+input_shape = (28, 28, 1)
 batch_size = 128
 num_classes = 10
 epochs = 10
